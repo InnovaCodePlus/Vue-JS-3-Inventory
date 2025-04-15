20b0a2c87bf0113d0accf9b7b6ae87f0c48ca1b9
Mattias Duarte <duartemattias3@gmail.com>
2025-04-14 21:09:25 -0400

    Agregando configuracion inicial

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import LoginView from "@/views/auth/LoginView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/auth/login",
+            name: "login",
+            component: LoginView
+        }
+    ],
+});
+
+
+export default router;
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+
+</script>
+
+<template>
+
+    <section class="flex items-center justify-center min-h-screen">
+        <div
+            class="mx-auto -z-10 absolute aspect-square rounded-full blur-[100px] -translate-y-2/3 bg-green-500/30 bottom-1/2 inset-0 max-w-5xl w-[65%]">
+        </div>
+
+        <form class="flex flex-col gap-4 border border-slate-300/20 w-full max-w-xl bg-black-soft rounded-md pt-7 pb-8 px-8">
+            <div>
+                <h1>Innova Inventory</h1>
+            </div>
+
+            <div>
+                <h2>Inciar Sesion</h2>
+                <p>Ingresa tus credenciales para acceder al sistema</p>
+            </div>
+            <Button>Iniciar Sesion</Button>
+            <!-- FORM FLIEDS -->
+
+        </form>
+    </section>
+
+
+</template>
